249dfb016230fe2e120d19b0fece79076a633942
Flamcom27 <27flamcom06@gmail.com>
2021-12-26 17:07:31 +0300

    templates: home, market are created

--- app.py
+++ app.py
@@ -9,9 +9,20 @@ def hello_world():  # put application's code here
     return render_template('home.html')
 
 
-@app.route('/about/<username>')  # dynamic route
-def about_page(username):
-    return f'<h1>about page {username}</1>'
+@app.route('/about')  # dynamic route
+def about_page():
+    return render_template('about.html')
+    # return f'<h1>about page {username}</1>'
+
+
+@app.route('/market')
+def market_page():
+    products = [
+        {'id': 1, 'name': 'phone', 'barcode': '379463', 'price': '500$'},
+        {'id': 2, 'name': 'laptop', 'barcode': '407852', 'price': '1000$'},
+        {'id': 3, 'name': 'keyboard', 'barcode': '869306', 'price': '100$'}
+    ]
+    return render_template('market.html', products=products)
 
 
 if __name__ == '__main__':
