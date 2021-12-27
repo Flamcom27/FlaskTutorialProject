fdf3fe8acf54cb10c7253f5bdcc9763cca39b2de
Flamcom27 <27flamcom06@gmail.com>
2021-12-27 10:03:51 +0300

    database is created

--- app.py
+++ app.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False)
+
+    def __repr__(self):
+        return self.name
 
 
 @app.route('/')
@@ -19,11 +31,7 @@ def about_page():
 
 @app.route('/market')
 def market_page():
-    products = [
-        {'id': 1, 'name': 'phone', 'barcode': '379463', 'price': '500$'},
-        {'id': 2, 'name': 'laptop', 'barcode': '407852', 'price': '1000$'},
-        {'id': 3, 'name': 'keyboard', 'barcode': '869306', 'price': '100$'}
-    ]
+    products = Product.query.all()
     return render_template('market.html', products=products)
 
 
