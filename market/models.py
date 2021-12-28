0fe0b69d245a6c9969912aa9c311261eaf75a63b
Flamcom27 <27flamcom06@gmail.com>
2021-12-28 23:28:27 +0300

    User Model is created

--- market/models.py
+++ market/models.py
@@ -1,12 +1,25 @@
 from market import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    products = db.relationship('Product', backref='owned_user', lazy=True)
+
+    def __repr__(self):
+        return self.name
+
+
 class Product(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return self.name
